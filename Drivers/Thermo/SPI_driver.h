12ff4f3e254e4e512c9c4d608b16697db8ff4d72
Zhogo <roussel.nathan@yahoo.fr>
2020-09-23 18:48:13 +0000

    SPI init WIP + need compare VS what is working (registers)

--- Drivers/Thermo/SPI_driver.h
+++ Drivers/Thermo/SPI_driver.h
@@ -4,12 +4,19 @@
 #include <stdio.h>
 #include "stm32f4xx.h"
 
-#define SPI1_GPIO GPIOC
+#define SPI1_GPIO GPIOA
+#define T_SPI SPI1
+
+//Alternate function configuration
+#define SPI1_CLK (0b0101<<(5*4))
+#define SPI1_MISO (0b0101<<(6*4))
+#define SPI1_MOSI (0b0101<<(7*4))
 
 /* PROTOTYPE */
 
 void spi_init(void);
 void spi_send(void);
+void spi_dma_init(void);
 
 
 #endif
